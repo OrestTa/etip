6a3bcae4fafb40ba50a992b2c85d0d7bd89458ce
orest@mytum.de <orest@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-05-23 23:56:24 +0000

    go.sh VPN check corrected...
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@5502 b9ba9295-1068-0410-8410-0f816a8a6c15

--- go.sh
+++ go.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-if [ nmcli con status | grep "LRZ IPsec ETI" | awk '{ print $1, $2, $3, $NF }' = "LRZ IPsec ETI yes"] ; then
+#!/bin/bash
+#if [ `nmcli con status | grep "LRZ IPsec ETI" | awk '{ print $1, $2, $3, $NF }'` != "LRZ IPsec ETI yes" ] ; then
+if ! nmcli con status | grep "LRZ IPsec ETI" ; then
 	nmcli con up id "LRZ IPsec ETI"
 fi
 git svn rebase
