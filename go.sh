1af75a121a71c093434c4534f8d24c7b2155b482
orest@mytum.de <orest@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-05-23 23:50:16 +0000

    go.sh: VPN check added
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@5501 b9ba9295-1068-0410-8410-0f816a8a6c15

--- go.sh
+++ go.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ nmcli con status | grep "LRZ IPsec ETI" | awk '{ print $1, $2, $3, $NF }' = "LRZ IPsec ETI yes"] ; then
+	nmcli con up id "LRZ IPsec ETI"
+fi
 git svn rebase
 git commit -a
 git svn dcommit
