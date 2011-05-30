f91004b16565d37cef415ebde414dd5bf22a2a6d
orest@mytum.de <orest@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-05-30 11:36:29 +0000

    go.sh updated for LRZ ETI
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@5542 b9ba9295-1068-0410-8410-0f816a8a6c15

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #if [ `nmcli con status | grep "LRZ IPsec ETI" | awk '{ print $1, $2, $3, $NF }'` != "LRZ IPsec ETI yes" ] ; then
-if ! nmcli con status | grep "LRZ IPsec ETI" ; then
+if ! nmcli con status | grep "LRZ IPsec" ; then
 	nmcli con up id "LRZ IPsec ETI"
 fi
 git svn rebase
