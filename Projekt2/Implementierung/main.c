a2568fba7700937038a69bbdeda59bb0c76bd420
lukas.maerdian@mytum.de <lukas.maerdian@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-06-25 17:36:14 +0000

    Project2: some documetation in the main.c file
    Signed-off-by: Lukas Märdian <lukasmaerdian@gmail.com>
    
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@5952 b9ba9295-1068-0410-8410-0f816a8a6c15

--- Projekt2/Implementierung/main.c
+++ Projekt2/Implementierung/main.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 
-/* prototype for the asm function */
+/*
+ * This extern (ASM) function calculates a matrix-vector-operation in form of:
+ * Y := ALPHA * A * X + BETA * Y
+ * where A is a matrix, ALPHA and BETA are scalars, X and Y are vectors
+ */
 extern int dgbmv(char trans, int m, int n, int kl, int ku, double *alpha, \
               double (*a)[], int lda, double (*x)[], int incx, double *beta, \
               double (*y)[], int incy);
 
+/*
+ * This function prints a vector of doubles in the form of:
+ * X: (1.0, 2.0, 3.0, ...)
+ */
 void printv(char name, double vector[], int inc, int size) {
-        int i;
-        int length = size/sizeof(double);
-        printf("%c    : (", name);
-        for(i=0; i<length; i=i+inc) {
-            printf("%f", vector[i]);
-            if( i < length-1 ) printf(", ");
-            }
-        printf(")\n");
+    int i;
+    int length = size/sizeof(double);
+    printf("%c    : (", name);
+    for(i=0; i<length; i=i+inc) {
+        printf("%f", vector[i]);
+        if( i < length-1 ) printf(", ");
+        }
+    printf(")\n");
     }
 
+/*
+ * This function prints an error code of dgbmv(), as of:
+ * -1   : INVALID_TRANS
+ * -2   : INVALID_M
+ * -3   : INVALID_N
+ * -4   : INVALID_KL
+ * -5   : INVALID_KU
+ * ...
+ */
+void printe(int error) {
+    printf("ERROR: %i\n", error);
+    }
+
+/*
+ * This is the program's starting point.
+ */
 int main(int argc, char *argv[]) {
-    /* define the parameters */
+
+    /* define the INPUT parameters here */
     char trans   = 'n';
     int  m       = 5;
     int  n       = 5;
@@ -32,6 +57,7 @@ int main(int argc, char *argv[]) {
     double y[]   = {6.0,0.0,7.0,0.0,8.0,0.0,9.0,0.0,0.0};
     int incy     = 2;
 
+    /* print the INPUT paramters */
     printf("\n");
     printf("INPUT Parameters:\n");
     printf("TRANS: %c\n", trans);
@@ -49,7 +75,7 @@ int main(int argc, char *argv[]) {
     printf("INCY : %i\n", incy);
     printf("\n");
 
-    /* call the actual asm function */
+    /* call the actual ASM function */
     int result = dgbmv(trans, m, n, kl, ku, &alpha, &a, lda, &x, incx, \
                        &beta, &y, incy);
 
@@ -60,14 +86,15 @@ int main(int argc, char *argv[]) {
         }
     else {
         printf("OUTPUT Error:\n");
-        printf("ERROR: %i\n", result);
+        printe(result);
     }
     printf("\n");
 
+    /* print the result (EAX) - for testing purposes only */
     printf("%i\n", sizeof(y[0]));
     printf("Result for testing\n");
     printf("hex: %x\n",result);
     printf("int: %i\n",result);
 
     return 0;
-}
+    }
