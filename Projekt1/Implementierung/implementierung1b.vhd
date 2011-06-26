8f94088b786a2657bec9f4e9263c646940d50bc8
mstaab@mytum.de <mstaab@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-06-26 17:35:38 +0000

    P1: Implementierung: beide architectures korrigiert; aktuelle architecture (V2) in separater Datei isoliert (implementierung1b.vhd)
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@5960 b9ba9295-1068-0410-8410-0f816a8a6c15

--- Projekt1/Implementierung/implementierung1b.vhd
+++ Projekt1/Implementierung/implementierung1b.vhd
@@ -13,54 +13,6 @@ END BINBCD;
 
 
 
-ARCHITECTURE DoubleDabbleV1 OF BINBCD IS
-
-	signal const : std_logic_vector (16 DOWNTO 0) := "11000011010011111";
-	signal vector : std_logic_vector (36 DOWNTO 0) := "0000000000000000000000000000000000000";
-	signal i : integer := 0;
-	
-BEGIN
-	
-	overflow <= '0';
-	
-	FOR i IN 0 TO 16 LOOP
-		IF (bin_input(i) = '1') AND (const(i) = '0') THEN
-			overflow <= '1';
-			EXIT
-		END IF;
-	END LOOP;
-	
-	
-	IF (overflow = "0") THEN
-	
-		FOR i IN 0 TO 16
-			vector(i+20) <= bin_input(i);
-		END LOOP;
-		
-		FOR i IN 0 TO 15 LOOP
-			vector sll 1;
-			-- Hierhin muss noch:
-			-- -Prüfen, ob vector(0to3), oder vector(4to7), oder vector(8to11), oder vector(12to15), oder vector(16to19) >= 5 sind
-			-- -Zu den jeweiligen Abschnitten von vector 3 addieren.
-			-- -Siehe hierfür architecture DoubleDabbleV2.
-		END LOOP;
-		vector sll 1;
-		
-		FOR i IN 0 TO 3
-			zehntausender(i) <= vector(i);
-			tausender(i) <= vector(i+4);
-			hunderter(i) <= vector(i+8);
-			zehner(i) <= vector(i+12);
-			einer(i) <= vector(i+16);
-		END LOOP;
-		
-	END IF;
-	
-	
-END DoubleDabbleV1;
-
-
-
 ARCHITECTURE DoubleDabbleV2 OF BINBCD IS
 
 	signal int_input : integer := 0;
@@ -121,7 +73,7 @@ BEGIN
 		
 	ELSE
 	
-		overflow <= '1'
+		overflow <= '1';
 	
 	END IF;
 	
