0865ce053405ee6304429eef9e40d31c01d2f49a
orest@mytum.de <orest@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-06-27 16:50:07 +0000

    p2 i2 comments, finish optimised test break line description test
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@5983 b9ba9295-1068-0410-8410-0f816a8a6c15

--- Projekt2/Implementierung/dgbmv.asm
+++ Projekt2/Implementierung/dgbmv.asm
@@ -6,32 +6,33 @@ global dgbmv
 %define N [EBP+16]
 %define KL [EBP+20]
 %define KU [EBP+24]
-%define _ALPHA [EBP+28]                 ; _X is X* (a pointer to X)
-%define _A [EBP+32]
+%define _ALPHA [EBP+28]                 ; _X means X* (a pointer to X)
+%define _A [EBP+32]                     ; *
 %define LDA [EBP+36]
-%define _X [EBP+40]
+%define _X [EBP+40]                     ; *
 %define INCX [EBP+44]
-%define _BETA [EBP+48]
-%define _Y [EBP+52]
+%define _BETA [EBP+48]                  ; *
+%define _Y [EBP+52]                     ; *
 %define INCY [EBP+56]
 
-; Multiply a scalar with a vector
-; you have to initalize like this:
-;               MOV ECX, 0      = counter
-;               MOV EDX, _Y     = vector*
-;               MOV EAX, _BETA  = scalar*
-;               MOV EBX, INCY   = inc
-;               MOV ESI, N      = length
+; Multiply a scalar with an incremented vector
+; Initalisation:
+;               MOV ECX, 0      : counter
+;               MOV EDX, _Y     : incremented vector*
+;               MOV EAX, _BETA  : scalar*
+;               MOV EBX, INCY   : increment of the vector
+;               MOV ESI, N      : length of the vector
 ;               CALL scalarmult
+
 %macro scalarmult 1
 %1:
-                FLD qword [EAX]
-                FLD qword [EDX]
-                FMUL
-                FSTP qword [EDX]
+                FLD qword [EAX]         ; first factor
+                FLD qword [EDX]         ; second factor
+                FMUL                    ; multiply!
+                FSTP qword [EDX]        ; save the result
 
-                ADD EDX, EBX
-                ADD EDX, EBX
+                ADD EDX, EBX            ; increase the pointer by 8*INC in order to
+                ADD EDX, EBX            ; make it point to the next element of the array
                 ADD EDX, EBX
                 ADD EDX, EBX
                 ADD EDX, EBX
@@ -39,26 +40,24 @@ global dgbmv
                 ADD EDX, EBX
                 ADD EDX, EBX
 
-                INC ECX
-                CMP ECX, ESI
-                JNE %1
-                
-                ;RET
+                INC ECX                 ; increase the counter
+                CMP ECX, ESI            ; check whether all elements have been processed
+                JNE %1                  ; if not, repeat
 %endmacro
 
-; The dgbmv function
+; DGBMV - PROLOGUE
 dgbmv:
-                push ebp                ; Prolog
-                mov ebp, esp
-; Check which operation we have to do
+                PUSH EBP
+                MOV EBP, ESP
+
+; Check which operation to execute
 transcheck:
-                MOV EAX, [EBP+8]        ; Check first Paramter TRANS
-                AND EAX, 0x000000ff     ; jump to notranspose, if it is N or n
-                CMP AL, 'N'             ; jump to transpose, if it is T,t,C or c
-                JE notranspose          ; jump to transerror, if it is neither
+                MOV EAX, TRANS          ; Check paramter TRANS
+                CMP AL, 'N'             ; jump to notranspose if it is N or n
+                JE notranspose
                 CMP AL, 'n'
                 JE notranspose
-                CMP AL, 'T'
+                CMP AL, 'T'             ; jump to transpose if it is T, t, C or c
                 JE transpose
                 CMP AL, 't'
                 JE transpose
@@ -66,28 +65,32 @@ transcheck:
                 JE transpose
                 CMP AL, 'c'
                 JE transpose
-                JMP transerror
+                JMP transerror          ; jump to transerror otherwise
 
 ; Transpose the matrix
 transpose:
                 mov eax, 10             ; EAX = 10 (just for testing)
-                JMP finish 
-notranspose:
-                MOV EAX, LDA            ; LDA muss in 16 Bit passen.
-                MUL word N              ; N muss in 16 Bit passen.
-                ADD EAX, EAX
-                PUSH EAX                ; Fehler ausgeben, falls zu groß.
-alloc:
-                PUSH dword 0x0
-                DEC EAX
-                JNZ alloc
-                MOV EAX, [EBP-4]
+                JMP finish              ; TODO! 
 
-; Copy matrix
-                MOV EDX, EBP
-                SUB EDX, EAX
-                SUB EDX, 4              ; EDX = neue Matrix*
-                MOV EBX, _A             ; EBX = alte Matrix*
+; Proceed without transposing the matrix
+notranspose:
+                MOV EAX, LDA            ; LDA has to fit in 16 bits!
+                MUL word N              ; N has to fit in 16 bits!; TODO: error
+                ADD EAX, EAX            ; duplicate EAX (each double needs 8 bytes instead of 4)
+                PUSH EAX
+
+; Allocate memory
+alloc:
+                PUSH dword 0x0          ; populate the stack with 0s to allow for dynamic management
+                DEC EAX                 ; do it EAX times, i. e. LDA * N * 2
+                JNZ alloc               ; repeat until EAX=0
+                MOV EAX, [EBP-4]        ; restore LDA * N * 2
+
+; Duplicate the matrix
+                MOV EDX, EBP            ; copy the base pointer
+                SUB EDX, EAX            ; calculate the position of the duplicated matrix (EDX)
+                SUB EDX, 4              ; populate the stack with duplicated matrix' elements
+                MOV EBX, _A             ; EBX contains the pointer to the original matrix
                 PUSH dword 0x0
 copy_loop:
                 MOV ECX, [EBX]
@@ -119,16 +122,20 @@ bsmvectmult:
 
 vectoradd:
 
-finish:
-                POP EBX                 ; Cleanup stack
-                CMP EBP, ESP
-                JNE finish
+; The function succeded 
+okay:
+                MOV EAX, 0              ; set return to 0
+                JMP finish
 
-                MOV EAX, 0x0
-                pop ebp                 ; Epilog
-                ret
-; Error cases
-transerror:                             ; TRANS is neither N,n,T,t,C nor c
-                mov eax, -1
-                pop ebp
-                ret
+; Errors
+transerror:                             ; TRANS is none of N, n, T, t, C, c
+                MOV EAX, -1             ; set return to -1
+                JMP finish
+
+; EPILOGUE
+finish:
+                POP EBX                 ; pop from the stack
+                CMP EBP, ESP            ; until it's empty
+                JNE finish
+                POP EBP
+                RET
