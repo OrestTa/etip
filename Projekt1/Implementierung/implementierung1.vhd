9272f8d2b83005b133f4859fc96f29ccceed27b4
orest@mytum.de <orest@mytum.de@b9ba9295-1068-0410-8410-0f816a8a6c15>
2011-07-03 23:47:40 +0000

    i1 retabbed!, comments translated and reviewed
    for retabbing, use :retab (replaces tabs with the current tab setting char(s))
    
    Signed-off-by: orest <orest@example.com>
    
    git-svn-id: svn://atbode71.informatik.tu-muenchen.de/etipraktikum/ss11-g07@6229 b9ba9295-1068-0410-8410-0f816a8a6c15

--- Projekt1/Implementierung/implementierung1.vhd
+++ Projekt1/Implementierung/implementierung1.vhd
@@ -15,12 +15,12 @@ END BINBCD;
 
 ARCHITECTURE DoubleDabbleV3 OF BINBCD IS
 
-SIGNAL overflw : std_logic := '1';									-- overflw standardmaessig auf 1
-																	-- Da der Port "overflow" im Prozess nicht ausgelesen werden kann,
-																	-- wird intern mit einem Signal "overflw" gearbeitet.
+SIGNAL overflw : std_logic := '1';                                          -- "overflw" is high per default
+                                                                            -- as the port "overflow" is OUT and cannot be read,
+                                                                            -- we will work with the signal "overflw"
 SIGNAL drei: unsigned(3 DOWNTO 0) := "0011";
-SIGNAL counter: unsigned(4 DOWNTO 0) := "00000";					-- um nach 17 Durchläufen abzubrechen
-SIGNAL linput: std_logic_vector(16 DOWNTO 0) := bin_input;
+SIGNAL counter: unsigned(4 DOWNTO 0) := "00000";                            -- in order to break after 17 cycles
+SIGNAL linput: std_logic_vector(16 DOWNTO 0) := bin_input;                  -- v. s.
 SIGNAL leiner: std_logic_vector(3 DOWNTO 0) := "0000";
 SIGNAL lzehner: std_logic_vector(3 DOWNTO 0) := "0000";
 SIGNAL lhunderter: std_logic_vector(3 DOWNTO 0) := "0000";
@@ -31,50 +31,53 @@ BEGIN
 
     PROCESS(clk)
     BEGIN
+
         IF (clk'EVENT) AND (clk = '1') AND (counter <= 17) AND (unsigned(bin_input) <= 99999) THEN
-																			-- Bei steigender Taktflanke, falls die Eingabe nicht zu gross ist,
-            lzehntausender <= lzehntausender(2 DOWNTO 0)&ltausender(3);		-- shifte alle BCD-Vektoren einmal nach links...
+                                                                            -- if rising edge and if input within domain
+            lzehntausender <= lzehntausender(2 DOWNTO 0)&ltausender(3);     -- shift left all BCD-vectors
             ltausender <= ltausender(2 DOWNTO 0)&lhunderter(3);
             lhunderter <= lhunderter(2 DOWNTO 0)&lzehner(3);
             lzehner <= lzehner(2 DOWNTO 0)&leiner(3);
             leiner <= leiner(2 DOWNTO 0)&linput(16);
-            linput <= linput(15 DOWNTO 0)&'0';								-- ...und zum Schluss auch den Eingabevektor.
-            counter <= counter+1;
+            linput <= linput(15 DOWNTO 0)&'0';                              -- and the input vector
+            counter <= counter+1;                                           -- 17 times
         END IF;
-        IF (clk'EVENT) AND (clk = '0') AND (counter <= 17) THEN				-- Bei fallender Taktflanke,
-        IF (unsigned(bin_input) <= 99999) THEN								-- falls die Eingabe nicht zu gross ist,
-            overflw <= '0';													-- setze "overflw" auf 0,
-            IF (unsigned(leiner) >= 5) THEN									-- prüfe jeweils für den BCD-Vektor der Einer
-                leiner <= std_logic_vector(unsigned(leiner)+drei);
+
+        IF (clk'EVENT) AND (clk = '0') AND (counter <= 17) THEN             -- if falling edge
+        IF (unsigned(bin_input) <= 99999) THEN                              -- if input within domain
+            overflw <= '0';                                                 -- set "overflw" to 0
+            IF (unsigned(leiner) >= 5) THEN                                 -- check which BCD vectors' values are equal to or bigger than 5
+                leiner <= std_logic_vector(unsigned(leiner)+drei);          -- and thus need adding 3; adjust them accordingly
             END IF;
-            IF (unsigned(lzehner) >= 5) THEN								-- Zehner
+            IF (unsigned(lzehner) >= 5) THEN
                 lzehner <= std_logic_vector(unsigned(lzehner)+drei);
             END IF;
-            IF (unsigned(lhunderter) >= 5) THEN								-- Hunderter
+            IF (unsigned(lhunderter) >= 5) THEN
                 lhunderter <= std_logic_vector(unsigned(lhunderter)+drei);
             END IF;
-            IF (unsigned(ltausender) >= 5) THEN								-- Tausender
+            IF (unsigned(ltausender) >= 5) THEN
                 ltausender <= std_logic_vector(unsigned(ltausender)+drei);
             END IF;
-            IF (unsigned(lzehntausender) >= 5) THEN							-- und Zehntausender, ob er >= 5 ist
-                lzehntausender <= std_logic_vector(unsigned(lzehntausender)+drei);	-- und addiere für diesen Fall drei.
+            IF (unsigned(lzehntausender) >= 5) THEN
+                lzehntausender <= std_logic_vector(unsigned(lzehntausender)+drei);
             END IF;
-														-- Aktualisiere die Ausgabeports mit den Inhalten der BCD-Vektoren.
+                                                                            -- update the output ports with the BCD vector signals
             einer <= leiner;
             zehner <= lzehner;
             hunderter <= lhunderter;
             tausender <= ltausender;
             zehntausender <= lzehntausender;
-        ELSE											-- Falls die Eingabe zu gross ist,
-            einer <= "0000";							-- setze alle Ausgabeports auf 0.
-            zehner <= "0000";
+        ELSE                                                                -- if the input is too large
+            einer <= "0000";                                                -- set the output to 0
+            zehner <= "0000";                                               -- (the OF bit is 1 per default)
             hunderter <= "0000";
             tausender <= "0000";
             zehntausender <= "0000";
         END IF;
         END IF;
+
     END PROCESS;
 
-    overflow <= overflw;								-- Uebertrage den Inhalt des internen Signals "overflw"
-														-- auf den Ausgabeport "overflow".
+    overflow <= overflw;                                                    -- update the port "overflow" with our internal signal "overflw"
+
 END DoubleDabbleV3;
